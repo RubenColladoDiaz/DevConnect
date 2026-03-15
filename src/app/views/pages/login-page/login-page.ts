98748f4fbe099a4c7895aca63b36dc475e01fb83
RUBEN COLLADO <ruben.co.diaz@gmail.com>
2026-03-15 21:58:44 +0100

    login post

--- src/app/views/pages/login-page/login-page.ts
+++ src/app/views/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,23 +10,33 @@ import {HttpClient} from '@angular/common/http';
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  loginForm: FormGroup;
 
-  loginForm:FormGroup;
+  message: string = '';
 
-  constructor(private fb:FormBuilder, private http:HttpClient) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
   }
 
-  onLogin(){
-    if(this.loginForm.invalid) return;
+  onLogin() {
+    if (this.loginForm.invalid) return;
 
-    this.http.post("http://localhost:3000/login", this.loginForm.value)
-      .subscribe((res: any) => {
-        localStorage.setItem("token", res.token);
-        localStorage.setItem("user", JSON.stringify(res.user));
-      });
+    this.http.post('http://localhost:3000/login', this.loginForm.value).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this.router.navigate(['/']);
+      },
+      error: (res: any) => {
+        this.message = res.error.message ?? 'Unexpected error';
+      },
+    });
   }
 }
