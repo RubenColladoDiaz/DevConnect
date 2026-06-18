73206a6fbc31aba3b1e0e9cc308b95852a0a96aa
Your Name <you@example.com>
2026-06-18 12:35:57 +0200

    updatePosts when creating Post in progress

--- src/app/views/components/new-post-template/new-post-template.ts
+++ src/app/views/components/new-post-template/new-post-template.ts
@@ -1,4 +1,4 @@
-import { Component, model, ModelSignal } from '@angular/core';
+import { ChangeDetectorRef, Component, model, ModelSignal } from '@angular/core';
 import { User } from '../../../shared/types/User';
 import { PostsService } from '../../../shared/services/posts-service';
 
@@ -10,11 +10,16 @@ import { PostsService } from '../../../shared/services/posts-service';
 })
 export class NewPostTemplate {
   user: User = JSON.parse(localStorage.getItem('user') || '{}');
+
   creating: ModelSignal<boolean> = model<boolean>(false);
+  getAllPosts = model<() => void>();
 
   content: string = '';
 
-  constructor(private postsService: PostsService) {}
+  constructor(
+    private postsService: PostsService,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   toggleCreating(): void {
     this.creating.update((creating) => !creating);
@@ -24,6 +29,8 @@ export class NewPostTemplate {
     this.postsService.createPost(this.content, []).subscribe({
       next: (res) => {
         this.toggleCreating();
+        this.getAllPosts();
+        this.cdr.detectChanges();
       },
       error: (err) => console.error('Error:', err),
     });
